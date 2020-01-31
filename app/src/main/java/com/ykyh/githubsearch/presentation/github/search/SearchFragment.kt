ca929c56f679e846f60e0c3a4067f166bf152bb5
Jacob Lee <ykyahwa@gmail.com>
2020-01-31 17:28:23 +0900

    ADD X 버튼 이벤트 추가

--- app/src/main/java/com/ykyh/githubsearch/presentation/github/search/SearchFragment.kt
+++ app/src/main/java/com/ykyh/githubsearch/presentation/github/search/SearchFragment.kt
@@ -2,9 +2,12 @@ package com.ykyh.githubsearch.presentation.github.search
 
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.paging.PagedList
 import com.ykyh.githubsearch.R
 import com.ykyh.githubsearch.data.GithubUserData
@@ -51,6 +54,22 @@ class SearchFragment @Inject constructor(): DaggerFragment(), GithubContract.Sea
         btSearch.setOnClickListener {
             presenter.searchUsers(etSearch.text.toString())
         }
+
+        btDelete.setOnClickListener {
+            etSearch.setText("")
+        }
+
+        etSearch.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val length = s?.length ?: 0
+                btDelete.isVisible = length > 0
+            }
+        })
+
 
     }
 
